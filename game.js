5d7137391dbad6b2f27d6b9df187cf3755928af7
mersanuzun <mersanuzun@gmail.com>
2015-08-22 14:41:12 +0300

    select board size

--- game.js
+++ game.js
@@ -2,6 +2,9 @@ var symbols = ["*", "&", "+", "#", "!", "|", "-", "/", "~", "<", ">", "@", "€"
                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "Q", "W", "Z", "S"
               ];
 var guess = 0;
+var cells;
+var click = 1;
+var correct = 0;
 function isExist (finished, coorX, coorY){
   var result;
   for (value in finished){
@@ -75,3 +78,53 @@ function controlCell(coor){
     }
   }
 }
+$("input:radio").on("click", function(){
+    boardSize = ($("input:radio:checked").val())
+    $(".selectBoardSize").hide();
+    cells = createBoard(boardSize);
+    var table = $("#game tbody");
+    for (var x = 0; x < boardSize; x++){
+        var row = $("<tr>");
+        for (var y = 0; y < boardSize; y++){
+            var cell = $("<td></td>")
+            cell.append("<div id='" + x + "" + y + "'style='background-color:#83F4EC; width:100%; height:100%;'>")
+            cell.data({'x' : x, 'y' : y});
+            row.append(cell);
+        }
+        table.append(row);
+    }
+    $("#game td").on("click", function(){
+    if (correct == (boardSize * boardSize) / 2) return
+    $that = $(this);
+    var xCoor = $that.data("x");
+    var yCoor = $that.data("y");
+    if (click != 2){
+        $("#" + xCoor + "" + yCoor).css("background-color", "#007871")
+        $("#" + xCoor + "" + yCoor).html(cells[xCoor][yCoor].symbol);
+        controlCell(cells[xCoor][yCoor]);
+        click++;
+    }else {
+        $("#" + xCoor + "" + yCoor).css("background-color", "#007871")                
+        $("#" + xCoor + "" + yCoor).html(cells[xCoor][yCoor].symbol);
+        var oldCoord = controlCell(cells[xCoor][yCoor]);
+        if (oldCoord instanceof Cell){
+            setTimeout(function(){
+                $("#" + oldCoord.coorX + "" + oldCoord.coorY).html("");
+                $("#" + xCoor + "" + yCoor).html("");
+                $("#" + xCoor + "" + yCoor).css("background-color", "#83F4EC");
+                $("#" + oldCoord.coorX + "" + oldCoord.coorY).css("background-color", "#83F4EC");
+            }, 1000)
+        }else {
+            correct++;
+            console.log(correct)
+        }
+        guess++;
+        console.log(guess)
+        click = 1;
+        if (correct == (boardSize * boardSize) / 2) {
+            $("#guess").text("Game is finished. You made " + guess + " guess.")
+        }
+    }
+});
+})
+
